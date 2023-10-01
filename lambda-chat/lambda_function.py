5d335c0079bb2c4e10548b913e1d26b7b2a5d016
ksdyb.park <ksdyb@amazon.com>
2023-10-01 15:16:43 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -298,19 +298,19 @@ def get_answer_using_template(query):
         print('word_kor: ', word_kor)
         
         if word_kor:
-            prompt_template = """\n\nHuman: 다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant는 모르는 질문을 받으면 솔직히 모른다고 말합니다.
+            prompt_template = """다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant는 모르는 질문을 받으면 솔직히 모른다고 말합니다.
         
             {context}
             
-            Human: {question}
+            Question: {question}
 
             Assistant:"""
         else:
-            prompt_template = """\n\nHuman: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+            prompt_template = """Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
             
             {context}
             
-            Human: {question}
+            Question: {question}
 
             Assistant:"""
         
