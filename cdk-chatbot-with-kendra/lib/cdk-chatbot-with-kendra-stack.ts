02bcb18aa2f0a326173b37290ec6b1ff142a7297
Kyoungsu Park <52392004+kyopark2014@users.noreply.github.com>
2023-12-13 22:32:52 +0900

    Update cdk-chatbot-with-kendra-stack.ts

--- cdk-chatbot-with-kendra/lib/cdk-chatbot-with-kendra-stack.ts
+++ cdk-chatbot-with-kendra/lib/cdk-chatbot-with-kendra-stack.ts
@@ -144,7 +144,16 @@ export class CdkChatbotWithKendraStack extends cdk.Stack {
         new iam.Policy(this, `kendra-log-policy-for-${projectName}`, {
           statements: [kendraLogPolicy],
         }),
-      );    
+      );
+    const kendraS3ReadPolicy = new iam.PolicyStatement({
+        resources: ['*'],
+        actions: ["s3:Get*","s3:List*","s3:Describe*"],
+      });
+      roleKendra.attachInlinePolicy( // add kendra policy
+        new iam.Policy(this, `kendra-s3-read-policy-for-${projectName}`, {
+          statements: [kendraS3ReadPolicy],
+        }),
+      );   
           
     const roleLambda = new iam.Role(this, `role-lambda-chat-for-${projectName}`, {
       roleName: `role-lambda-chat-for-${projectName}-${region}`,
